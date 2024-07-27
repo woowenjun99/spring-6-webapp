8a6270083ce3b7819c8cf2c2fc66cf6a762d0b0c
woowenjun99 <woowenjun99@gmail.com>
2024-07-27 22:54:10 +0800

    Complete section 2

--- src/main/java/guru/springframework/spring6webapp/domain/Book.java
+++ src/main/java/guru/springframework/spring6webapp/domain/Book.java
@@ -2,6 +2,7 @@ package guru.springframework.spring6webapp.domain;
 
 import jakarta.persistence.*;
 
+import java.util.HashSet;
 import java.util.Set;
 
 @Entity
@@ -19,7 +20,17 @@ public class Book {
             joinColumns = @JoinColumn(name = "book_id"),
             inverseJoinColumns = @JoinColumn(name = "author_id")
     )
-    private Set<Author> authors;
+    private Set<Author> authors = new HashSet<>();
+    @ManyToOne
+    private Publisher publisher;
+
+    public Publisher getPublisher () {
+        return publisher;
+    }
+
+    public void setPublisher (Publisher publisher) {
+        this.publisher = publisher;
+    }
 
     public Set<Author> getAuthors () {
         return authors;
